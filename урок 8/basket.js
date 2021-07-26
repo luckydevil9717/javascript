cc24ebc2c6c95523ed36ca04da1bfaaddad0e1fa
luckydevil9717 <63519935+luckydevil9717@users.noreply.github.com>
2021-07-26 17:35:47 +0300

    Add files via upload

---
+++ урок 8/basket.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const openBasketBtn = document.querySelector('.cartIconWrap');
+const basketEl = document.querySelector('.basket');
+const basketCounterEl = document.querySelector('.cartIconWrap span');
+const basketTotalEl = document.querySelector('.basketTotal');
+const basketTotalValueEl = document.querySelector('.basketTotalValue');
+
+openBasketBtn.addEventListener('click', function () {
+    basketEl.classList.toggle('hidden');
+});
+
+/**
+ * для хранения кол-во товара
+ */
+let basket = {};
+
+/**
+ * добавляет продукт в объект basket
+ */
+function addProductToObject(productId) {
+    if (!(productId in basket)) {
+        basket[productId] = 1;
+    } else {
+        basket[productId]++;
+    }
+}
+
+/**
+ * для отрисовки продукта в корзине
+ *
+ */
+function renderProductInBasket(productId) {
+    let productExist = document.querySelector(`.productCount[data-productId="${productId}"]`);
+    if (productExist) {
+        increaseProductCount(productId);
+        recalculateSumForProduct(productId);
+    } else {
+        renderNewProductInBasket(productId);
+    }
+}
+
+/**
+ * для отрисовывки нового товара в корзине
+ */
+function renderNewProductInBasket(productId) {
+    let productRow = `
+        <div class="basketRow">
+            <div>${products[productId].name}</div>
+            <div>
+                <span class="productCount" data-productId="${productId}">1</span> шт.
+            </div>
+            <div>$${products[productId].price}</div>
+            <div>
+                $<span class="productTotalRow" data-productId="${productId}">${products[productId].price}</span>
+            </div>
+        </div>
+    `;
+    basketTotalEl.insertAdjacentHTML("beforebegin", productRow);
+}
+
+/**
+ *  увеличивает количество товаров в корзине
+ */
+function increaseProductCount(productId) {
+    const productCountEl = document.querySelector(`.productCount[data-productId="${productId}"]`);
+    productCountEl.textContent++;
+}
+
+/**
+ * считает стоимость товара в корзине
+ * @param {number} productId
+ */
+function recalculateSumForProduct(productId) {
+    const productTotalRowEl = document.querySelector(`.productTotalRow[data-productId="${productId}"]`);
+    let totalPriceForRow = (basket[productId] * products[productId].price).toFixed(2);
+    productTotalRowEl.textContent = totalPriceForRow;
+}
+
+/**
+ * считает общую стоимость корзины и выводит
+ */
+function calculateAndRenderTotalBasketSum() {
+    let totalSum = 0;
+    for (let productId in basket) {
+        totalSum += basket[productId] * products[productId].price;
+    }
+    basketTotalValueEl.textContent = totalSum.toFixed(2);
+}
+
+/**
+ * увеличивает количества товаров рядом с иконкой корзины
+ */
+function increaseProductsCount() {
+    basketCounterEl.textContent++;
+}
+
+/**
+ * для добавления нового товара в корзину
+ */
+function addProductIntoBasket(productId) {
+    increaseProductsCount();
+    addProductToObject(productId);
+    renderProductInBasket(productId);
+    calculateAndRenderTotalBasketSum();
+}
+
